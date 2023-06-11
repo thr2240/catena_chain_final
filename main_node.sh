c4d7b4cd6c6fd87a10de25985b8b8160b97fd355
thr2240 <thomasr2240@gmail.com>
2023-06-12 00:45:46 +0800

    add for centry node and change some parameters

--- main_node.sh
+++ main_node.sh
@@ -67,15 +67,16 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 	jq '.app_state["inflation"]["params"]["mint_denom"]="exa"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 
 	# Set gas limit in genesis
-	jq '.consensus_params["block"]["max_gas"]="2000000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-	jq '.consensus_params["block"]["max_bytes"]="104857600"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+	jq '.consensus_params["block"]["max_gas"]="2100000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+	jq '.consensus_params["block"]["max_bytes"]="67108864"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+	jq '.consensus_params["evidence"]["max_bytes"]="67108864"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 
 	# Set base_fee in genesis
-	jq '.app_state["feemarket"]["params"]["base_fee"]="1000000000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-	jq '.app_state["feemarket"]["params"]["min_gas_price"]="1000000000000.000000000000000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+	jq '.app_state["feemarket"]["params"]["base_fee"]="10000000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+	jq '.app_state["feemarket"]["params"]["min_gas_price"]="10000000000.000000000000000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 
 	# Set min_deposit for proposal in genesis
-	jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["amount"]="1000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+	jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["amount"]="1000000000000000000000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 	jq '.app_state["gov"]["deposit_params"]["max_deposit_period"]="259200s"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 	jq '.app_state["gov"]["voting_params"]["voting_period"]="259200s"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 
@@ -122,12 +123,31 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 
 	sed -i 's/address = "127.0.0.1:8545"/address = "0.0.0.0:8545"/g' ~/.catenad/config/app.toml
 	sed -i 's/ws-address = "127.0.0.1:8546"/ws-address = "0.0.0.0:8546"/g' ~/.catenad/config/app.toml
+	sed -i 's/tracer = ""/tracer = "json"/g' ~/.catenad/config/app.toml
+	
+	######### for security##########
+	# sed -i 's/pex = true/pex = false/g' "$CONFIG"
+	# sed -i 's/addr_book_strict = true/addr_book_strict = false/g' "$CONFIG"
+	# sed -i 's/persistent_peers = ""/persistent_peers = "12382423f6680740565fd04137278751bd7a57af@47.74.6.60:26656"/g' "$CONFIG"
 
+	# sed -i 's/laddr = "tcp:\/\/0.0.0.0:26656"/laddr = "tcp:\/\/127.0.0.1:26656"/g' "$CONFIG"
+
+	# sed -i 's/address = "tcp:\/\/0.0.0.0:1317"/address = "tcp:\/\/127.0.0.1:1317"/g' ~/.catenad/config/app.toml
+	# sed -i 's/address = "0.0.0.0:9090"/address = "127.0.0.1:9090"/g' ~/.catenad/config/app.toml
+	# sed -i 's/address = "0.0.0.0:9091"/address = "127.0.0.1:9091"/g' ~/.catenad/config/app.toml
+
+	# ########## following https://docs.evmos.org/validate/security/validator-security-checklist for validator node####
+	# sed -i 's/max_num_inbound_peers = 240/max_num_inbound_peers = 100/g' "$CONFIG"
+	# sed -i 's/max_num_outbound_peers = 30/max_num_outbound_peers = 10/g' "$CONFIG"
+		
+	#####################
+	
 	sed -i '/\[api\]/,+3 s/enable = false/enable = true/' ~/.catenad/config/app.toml
 	sed -i 's/swagger = false/swagger = true/g' ~/.catenad/config/app.toml
 	sed -i 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/g'  ~/.catenad/config/app.toml
 	sed -i 's/enable-indexer = false/enable-indexer = true/g' ~/.catenad/config/app.toml
 	sed -i 's/api = "eth,net,web3"/api = "eth,txpool,personal,net,debug,web3,pubsub,trace"/g' ~/.catenad/config/app.toml
+	sed -i 's/pruning = "default"/pruning = "nothing"/g' ~/.catenad/config/app.toml
 
 	# Allocate genesis accounts (cosmos formatted addresses)
 	
@@ -140,7 +160,7 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 
 
 	# Sign genesis transaction
-	catenad gentx ${KEYS[0]} 6400000000000000000000000exa --keyring-backend $KEYRING --chain-id $CHAINID --home "$HOMEDIR" --fees 200000000000000000exa --min-self-delegation 6400000
+	catenad gentx ${KEYS[0]} 6400000000000000000000000exa --keyring-backend $KEYRING --chain-id $CHAINID --home "$HOMEDIR"  --fees 2000000000000000exa --min-self-delegation 6400000
 
 
 
@@ -155,6 +175,6 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 	fi
 fi
 
-# catenad start --pruning=nothing "$TRACE" --gas-prices 20000exa --gas-adjustment 1.3 --fees auto --rpc.laddr tcp://0.0.0.0:26657 --log_level $LOGLEVEL --json-rpc.api eth,txpool,personal,net,debug,web3 --api.enable --home "$HOMEDIR"
+#  catenad start --pruning=nothing "$TRACE" --gas-prices 20000exa --gas-adjustment 1.3 --fees auto --rpc.laddr tcp://0.0.0.0:26657 --log_level $LOGLEVEL --json-rpc.api eth,txpool,personal,net,debug,web3 --api.enable --home "$HOMEDIR"
 
 # catenad tx staking create-validator --amount=1000000000000000000000exa --from=validator2 --pubkey=$(catenad tendermint show-validator) --moniker="validator2" --chain-id catena_2121-1 --commission-rate="0.1" --commission-max-rate="0.2" --commission-max-change-rate="0.05" --min-self-delegation="500000000" --keyring-backend=test --yes --broadcast-mode block
